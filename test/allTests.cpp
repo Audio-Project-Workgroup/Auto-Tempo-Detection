6de63896378234a8270d321838129b62033b61e7
melissaspaschalis@gmail.com <melissaspaschalis@gmail.com>
2025-03-08 10:43:04 +0200

    add conditional build for tests

--- test/allTests.cpp
+++ test/allTests.cpp
@@ -1,5 +1,11 @@
-#include "src/audioProcessorTests.cpp"
-#include "src/mainComponentTests.cpp"
+#define JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED // https://forum.juce.com/t/error-no-global-header-file-was-included/19843
+
+#ifdef BUILD_PLUGIN_TESTS
+  #include "pluginTests.h"
+#endif
+#ifdef BUILD_APP_TESTS
+  #include "applicationTests.h"
+#endif
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
---
+++ test/applicationTests.h
@@ -0,0 +1,1 @@
+#include "src/mainComponentTests.cpp"
