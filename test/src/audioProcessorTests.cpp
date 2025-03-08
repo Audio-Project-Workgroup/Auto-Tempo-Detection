ea25ef2c8a571be072ce3ebf1509cc14ae5957ac
melissaspaschalis@gmail.com <melissaspaschalis@gmail.com>
2025-03-08 21:38:10 +0200

    clean up test files - remove unnecessary comments and defines

--- test/src/audioProcessorTests.cpp
+++ test/src/audioProcessorTests.cpp
@@ -1,17 +1,12 @@
 #include <gtest/gtest.h>
-#define JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED // https://forum.juce.com/t/error-no-global-header-file-was-included/19843
 #include "AudioProjectWorkgroup/PluginProcessor.h"
           
-
-namespace audio_plugin_test {
-
-    TEST(PLUGIN, InitAudioPluginAudioProcessor){
-        try {
-            AudioPluginAudioProcessor processor{};
-        } catch (const std::exception& e) {
-            FAIL() << "Runtime error during initialization: " << e.what();
-        } catch (...) {
-            FAIL() << "Unknown error during initialization";
-        }
+TEST(PLUGIN, InitAudioPluginAudioProcessor){
+    try {
+        AudioPluginAudioProcessor processor{};
+    } catch (const std::exception& e) {
+        FAIL() << "Runtime error during initialization: " << e.what();
+    } catch (...) {
+        FAIL() << "Unknown error during initialization";
     }
 }
--- test/src/mainComponentTests.cpp
+++ test/src/mainComponentTests.cpp
@@ -1,17 +1,12 @@
 #include <gtest/gtest.h>
-// #define JUCE_GLOBAL_MODULE_SETTINGS_INCLUDED // https://forum.juce.com/t/error-no-global-header-file-was-included/19843
 #include "AudioProjectWorkgroup/MainComponent.h"
           
-
-namespace audio_app_test {
-
-    TEST(APPLICATION, InitMainComponent){
-        try {
-            MainComponent component();
-        } catch (const std::exception& e) {
-            FAIL() << "Runtime error during initialization: " << e.what();
-        } catch (...) {
-            FAIL() << "Unknown error during initialization";
-        }
+TEST(APPLICATION, InitMainComponent){
+    try {
+        MainComponent component();
+    } catch (const std::exception& e) {
+        FAIL() << "Runtime error during initialization: " << e.what();
+    } catch (...) {
+        FAIL() << "Unknown error during initialization";
     }
 }
